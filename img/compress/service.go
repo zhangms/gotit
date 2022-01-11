23b65818672123bdc0fd8689d4d2f0bda0a517cf
maosheng.zms <maosheng.zms@alibaba-inc.com>
2022-01-11 19:34:54 +0800

    x

--- img/compress/service.go
+++ img/compress/service.go
@@ -19,6 +19,7 @@ func usage() {
 	msg = append(msg, "STEP3: 在workspace下新建文本文档 apikey.txt，将 STEP2 获取到的API key 写入 apikey.txt 第一行")
 	msg = append(msg, "STEP4: 在workspace下新建文件夹 compress, 将需要压缩的图片或文件夹放入其中")
 	msg = append(msg, "STEP5: 将本程序放入 workspace，双击运行，压缩后的文件夹将放入 compress_new 内")
+	msg = append(msg, "NOTE:  一个 tinypng 账号默认只能一个月免费压缩500张图片，超过500张可以付费或者换个邮箱获取apikey")
 	msg = append(msg, "")
 	msg = append(msg, "最终目录结构如下：")
 	msg = append(msg, "")
