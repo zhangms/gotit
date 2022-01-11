6fbd38308dccc9d2cdb709afbec049ab01c23ee7
maosheng.zms <maosheng.zms@alibaba-inc.com>
2022-01-11 14:04:49 +0800

    x

--- main.go
+++ main.go
@@ -8,23 +8,28 @@ import (
 	"gotit/watermark"
 	"os"
 	"os/signal"
+	"path/filepath"
 )
 
 func main() {
 	action := flag.String("action", "watermark", "watermark")
-	workspace := flag.String("workspace", ".", "workspace")
-	routine := flag.Int("routine", 10, "routine count")
+	workspace := flag.String("workspace", filepath.Dir(os.Args[0]), "workspace")
+	routine := flag.Int("routine", 16, "routine count")
 	flag.Parse()
 	switch *action {
 	case "watermark":
 		watermark.Usage()
 		watermark.DoMark(*workspace, *routine)
 	}
-
 	fmt.Println("----------------------")
 	fmt.Println("按 ctrl + c 退出")
 	c := make(chan os.Signal)
 	signal.Notify(c)
-	s := <-c
-	fmt.Println("stop,signal : ", s)
+	for true {
+		sig := <-c
+		if "interrupt" == sig.String() {
+			os.Exit(0)
+		}
+	}
+
 }
