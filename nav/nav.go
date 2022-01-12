74333d5a65bc8f67be6121e93c51e9150e29caea
maosheng.zms <maosheng.zms@alibaba-inc.com>
2022-01-12 16:37:14 +0800

    x

--- nav/nav.go
+++ nav/nav.go
@@ -45,45 +45,67 @@ func init() {
 }
 
 func Start(args ...string) {
-	printHeader := func() {
-		fmt.Printf("\n")
-		for i, n := range navigates {
-			fmt.Printf("输入%2d 执行:%s\n", i+1, n.name)
-		}
-		fmt.Printf("输入 q 退出\n")
-	}
-
-	scanInput := func() string {
-		fmt.Printf("Opt>")
-		var input string
-		_, _ = fmt.Scanln(&input)
-		return strings.TrimSpace(input)
-	}
-
 	printHeader()
 	for {
 		input := scanInput()
-		switch input {
-		case "q":
-			os.Exit(0)
-		case "":
+		if len(input) == 0 {
 			continue
-		default:
-			index, err := strconv.Atoi(input)
-			if err != nil || index < 1 || index > len(navigates) {
-				fmt.Println("请输入正确的数字")
-				continue
-			}
-			n := navigates[index-1]
-			fmt.Println("开始执行：", n.name, "(姜姜出品)")
-			time.Sleep(time.Second)
-			err = n.action(args)
-			if err != nil {
-				fmt.Println(err)
-				time.Sleep(time.Second)
-				n.usage()
-				printHeader()
-			}
 		}
+		if "q" == input {
+			os.Exit(0)
+			return
+		}
+		if strings.HasPrefix(input, "h") {
+			usage(input)
+			continue
+		}
+		exec(input, args)
 	}
+}
+
+func exec(input string, args []string) {
+	index, err := strconv.Atoi(input)
+	if err != nil || index < 1 || index > len(navigates) {
+		fmt.Println("请输入正确的数字")
+		return
+	}
+	n := navigates[index-1]
+	fmt.Println("开始执行：", n.name, "(姜姜出品)")
+	time.Sleep(time.Second)
+	err = n.action(args)
+	if err != nil {
+		fmt.Println(err)
+		time.Sleep(time.Second)
+		n.usage()
+	}
+	printHeader()
+}
+
+func usage(input string) {
+	input = string([]rune(input)[1:])
+	index, err := strconv.Atoi(input)
+	if err != nil || index < 1 || index > len(navigates) {
+		fmt.Println("请输入正确的数字")
+		printHeader()
+		return
+	}
+	n := navigates[index-1]
+	fmt.Println(n.name)
+	n.usage()
+}
+
+func scanInput() string {
+	fmt.Printf("Opt>")
+	var input string
+	_, _ = fmt.Scanln(&input)
+	return strings.TrimSpace(input)
+}
+
+func printHeader() {
+	fmt.Printf("\n")
+	for i, n := range navigates {
+		fmt.Printf("输入%2d 执行:%s\n", i+1, n.name)
+	}
+	fmt.Printf("输入 h+数字 查看数字对应功能的帮助信息，例如: h1\n")
+	fmt.Printf("输入 q 退出\n")
 }
